6153e6ef8235f743d75b96f258fe8c3e262b682c
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-08-06 10:15:12 -0400

    Configure CORS settings to allow all origins and credentials

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True #If True, all origins will be allowed. Other settings restricting allowed origins will be ignored. Defaults to False.
+CORS_ALLOW_CREDENTIALS = True #If True, cookies will be allowed to be included in cross-site HTTP requests. 
