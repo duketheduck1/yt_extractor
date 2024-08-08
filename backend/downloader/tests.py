66a560264ac6df19a21683ebbef9b618c96199c2
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-08-08 11:45:42 -0400

    test load ytdlp data

--- backend/downloader/tests.py
+++ backend/downloader/tests.py
@@ -4,19 +4,50 @@ from django.test import TestCase
 import json
 import yt_dlp
 
-URL = 'https://www.youtube.com/watch?v=bqNvkAfTvIc'
+# Test ytdlp output meta data to file
+def output_to_file(url: str):
+    ydl_opts = {}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        meta = ydl.extract_info(url, download=False)
 
-ydl_opts = {}
+        # Extract the formats list
+        formats = meta.get('formats', [])
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    info = ydl.extract_info(URL, download=False)
+        #save all formats to test.txt file
+        if formats:
+            with open("test.txt", "w") as file:
+                for line in formats:
+                    file.write(json.dumps(line, indent=2)+"\n\n")
+        else:
+            print("No formats available")
 
-    # Extract the formats list
-    formats = info.get('formats', [])
+# load ytdlp data       
+def load_specific_data(url: str):
+    ydl_opts = {}
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            meta = ydl.extract_info(url, download=False)
 
-    if formats:
-        with open("test.txt", "w") as file:
-            for line in formats:
-                file.write(json.dumps(line, indent=2)+"\n\n")
-    else:
-        print("No formats available")
+            # Extract the formats list
+            video_stream = []
+            formats = meta.get("formats", [])
+            for i in formats:
+                resolution = "audio only"
+                if i["height"] != None:
+                    resolution = f"{i["height"]} x {i["width"]}"
+                video_stream.append(
+                    {
+                        "resolution": resolution,
+                        "size": i["filesize"],
+                        "url": url,
+                    }
+                )
+            for i in video_stream:
+                print(i)
+       
+    except Exception:
+        print("There is no such url")
+
+if __name__ == "__main__":
+    URL = 'https://www.youtube.com/watch?v=bqNvkAfTvIc'
+    load_specific_data(URL)
