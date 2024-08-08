c650b57a30df21e23b3ef3ed93efd1bd11f6d076
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-08-08 09:31:19 -0400

    testing ytdlp method

--- backend/downloader/tests.py
+++ backend/downloader/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+import json
+import yt_dlp
+
+URL = 'https://www.youtube.com/watch?v=bqNvkAfTvIc'
+
+ydl_opts = {}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    info = ydl.extract_info(URL, download=False)
+
+    # Extract the formats list
+    formats = info.get('formats', [])
+
+    if formats:
+        with open("test.txt", "w") as file:
+            for line in formats:
+                file.write(json.dumps(line, indent=2)+"\n\n")
+    else:
+        print("No formats available")
