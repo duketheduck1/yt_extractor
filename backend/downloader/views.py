dc57a9a972f975d738855922b1ba2f568a3e7100
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-08-09 10:20:25 -0400

    add underscore

--- backend/downloader/views.py
+++ backend/downloader/views.py
@@ -6,13 +6,14 @@ import re
 import yt_dlp
 
 
-def validate_youtube_url(url: str) -> bool:
-    
+def _validate_youtube_url(url: str) -> bool:
     if isinstance(url, str) == True:
+        #check if url is a normal youtube url
         if "shorts" not in url:
             yt_regex = "^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
             youtube_match = re.match(yt_regex, url)
             return youtube_match is not None
+        #check if url is short youtube url
         elif "shorts" in url:
             short_regex = "^(?:https?:\/\/)?(?:www\.)?(youtube\.com\/(watch\?v=|shorts\/)|youtu\.be\/)([\w\-]+)"
             short_match = re.match(short_regex, url)
