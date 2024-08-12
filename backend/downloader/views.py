479fcb7ffec02cae48e918443788960b09a8aba7
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-08-12 10:08:45 -0400

    update view.py

--- backend/downloader/views.py
+++ backend/downloader/views.py
@@ -1,11 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+# from django.http import HttpResponse
 import json
 import re
 import yt_dlp
 
-
+# matching youtube url regex
 def _validate_youtube_url(url: str) -> bool:
     if isinstance(url, str) == True:
         #check if url is a normal youtube url
@@ -20,16 +21,23 @@ def _validate_youtube_url(url: str) -> bool:
             return short_match is not None
     else:
         return False
+
 @api_view(["POST"])
 def load_youtube_data(request):
     try:
         print("Loading video from youtube")
         url = request.data.get('url')
+        
         if _validate_youtube_url(url):
             pass
+        else:
+            return Response("Please input correct url.", status=400)
+    except Exception as e:
+        return Response(e.args[0], status=400)
+    
+def _get_youtube_stream(url: str):
+    pass
 
-    except:
-        pass
 
 
 
