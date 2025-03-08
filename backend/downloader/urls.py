2e7247888dbedaf32959d4b0407c0b730f23b1a1
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2025-03-07 21:23:17 -0500

    update urls path

--- backend/downloader/urls.py
+++ backend/downloader/urls.py
@@ -4,4 +4,5 @@ from downloader.views import *
 
 urlpatterns = [
     path("load-meta-data/",load_youtube_data, name="load_youtube_data"),
+    path("download/<str:filename>/",download_file, name="download_file"),
 ]
