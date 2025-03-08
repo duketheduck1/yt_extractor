14be3aa842eebe8c0ac664bc4c9a32da40cf0ec8
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2025-03-07 21:25:46 -0500

    update download

--- backend/downloader/views.py
+++ backend/downloader/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from datetime import timedelta
 from django.http import HttpResponse
+from django.conf import settings
+import os
 import json
 import re
 import yt_dlp
@@ -68,7 +70,7 @@ def _list_yt_resolution(formats: list) -> list:
     res_basic = ["1440", "1080", "720", "480", "360", "240"]
     for format in formats:
         resolution = format.get('format_note')
-        if resolution and resolution[0].isdigit() and resolution in res_basic:
+        if resolution and resolution[0].isdigit():# and resolution in res_basic:
             resolutions.add(resolution)
 
     list_resolution_string = sorted(list(resolutions))
@@ -99,5 +101,76 @@ def download_video(url, resolution_choice):
         ydl.download(url)
 
 
+@api_view(['GET'])
+def download_file(request, filename):
+    url, res, ex_file = make_info(filename)
+    try:
+        # Check URL validity and file extension
+        if _validate_youtube_url(url) and (ex_file in 'mp4|mp3'):
+            file_path = os.path.join(settings.MEDIA_ROOT, filename)
+            ex_file = filename[-3:]
 
+            # If the file doesn't exist, try to download it
+            if not os.path.exists(file_path):
+                get_video(url, res, ex_file, filename)
+
+            # Check again if the file exists after the download attempt
+            if os.path.exists(file_path):
+                with open(file_path, 'rb') as f:
+                    response = HttpResponse(f.read(), content_type=f'application/{ex_file}')
+                    response['Content-Disposition'] = f'attachment; filename="{filename}"'
+                    return response
+            else:
+                # Return a 404 response if the file is still not found
+                return Response({"error": "File not found"}, status=404)
+        else:
+            # Return a 400 response for an invalid URL or file extension
+            return Response({"error": "Invalid URL or file extension"}, status=400)
+
+    except Exception as e:
+        # Catch any other exceptions and return a 500 Internal Server Error
+        return Response({"error": str(e)}, status=500)    
+
+def get_video(url, res, ex_file, filename, max_retries=3, delay=5):
+    
+    file_path = os.path.join(settings.MEDIA_ROOT, filename)
+
+    # Configure yt-dlp options for video or audio downloads
+    if ex_file == 'mp3':
+        ydl_opts = {
+            'format': 'm4a/bestaudio/best',            
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': ex_file,
+            }],
+            'outtmpl': file_path[0:len(file_path)-4],
+            'retries': max_retries  # yt-dlp retry option
+        }
+    else:
+        ydl_opts = {
+            "format": f"bestvideo[height<={res}]+bestaudio/best",  # Best video and audio
+            "merge_output_format": ex_file,  # Merge into mp4, mkv, etc.
+            "outtmpl": file_path,  # Output path
+            "postprocessors": [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': ex_file,  # Convert to specific format (mp4, mkv, etc.)
+            }],
+            'socket_timeout': 60,  # Set socket timeout to 60 seconds
+        }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(url, download=True)
+        file_path = ydl.prepare_filename(result)
+        return file_path
+
+
+
+def make_info(filename):
+
+    ex_file = filename[-3:]
+    lenfile = len(filename)
+    url ="https://www.youtube.com/watch?v="+filename[1:lenfile-8]
+    
+    res = int(filename[lenfile-8:lenfile-4])  
+    return url,res,ex_file
 
