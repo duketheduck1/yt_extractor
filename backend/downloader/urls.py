aecc000691295eb35170c2eb660ddbcdb5d0442c
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2025-03-24 20:10:17 -0400

    change function for URL matching pattern

--- backend/downloader/urls.py
+++ backend/downloader/urls.py
@@ -4,5 +4,5 @@ from downloader.views import *
 
 urlpatterns = [
     path("load-meta-data/",load_youtube_data, name="load_youtube_data"),
-    path("download/<str:filename>/",download_file, name="download_file"),
+    path("download/<str:filename>/",download_media, name="download_file"),
 ]
