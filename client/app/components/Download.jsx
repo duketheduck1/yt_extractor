56390f4c6e394dfb07d1509e83a62c414c33a8fa
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2025-04-02 23:19:45 -0400

    add gitignore

---
+++ client/app/components/Download.jsx
@@ -0,0 +1,12 @@
+import { FormEvent, useState } from 'react';
+import axios from 'axios';
+
+const Download = ({ onMetadataReceived }) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Download;
---
+++ client/app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from '../components/Nav.jsx';
+import Footer from '../components/Footer.jsx';
+import Home from '../components/Home.jsx';
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
