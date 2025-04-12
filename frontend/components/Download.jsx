4737f33f88872fa330ba40cedec45e1ff7c53798
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2025-04-11 22:38:34 -0400

    update download.jsx, index.css

--- frontend/components/Download.jsx
+++ frontend/components/Download.jsx
@@ -1,12 +1,14 @@
-// import { FormEvent, useState } from 'react';
-// import axios from 'axios';
+import { FormEvent, useState } from 'react';
+import axios from 'axios';
 
-// const Download = ({ onMetadataReceived }) => {
-//     return (
-//         <div>
-            
-//         </div>
-//     );
-// };
+const Download = ({ onMetadataReceived }) => {
+    const [format, setFormat] = useState('video');
+    const [ resolution, setResolution] = useState(MediaMetadata.resolution[0]);
+    const [ fileFormat, setFileFormat] = useState(MediaMetadata.resolution[0]);
+    const [status, setStatus] = useState('');
+    const [loading, setLoading] = useState(false);
 
-// export default Download;
+
+    
+
+export default Download;
