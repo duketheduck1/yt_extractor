9e80decced53a9aeee9199371ead95b6f5127bc8
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2025-04-20 21:39:55 -0400

    refactor Home component layout for improved styling and structure

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -5,9 +5,10 @@ import Searchbar from "./Searchbar";
 const Home = () => {
     return (
         <>
-            <section className="px-6 md:px-20 py-40 border-2 border-red-500">
-                <div className="flex max-xl:flex-col gap-16">
-                    <div className="flex flex-col justify-center"> 
+            <section className="px-6 md:px-20 py-40 border-2 border-red-500 top-0">
+                <div className="flex flex-col gap-4 top-1">
+                    {/* flex-col move content to left */}
+                    <div className="flex flex-col justify-center top-1"> 
                         <p class="small-text">
                             Please Input your Youtube url
                         </p>
